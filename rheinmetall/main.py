5eebc20e3d1e50d0fa4d5353fbc67aebecda2cb3
Vito Genovese <genovese.work@protonmail.com>
2023-10-03 20:44:29 +0200

    jaime

--- rheinmetall/main.py
+++ rheinmetall/main.py
@@ -1,37 +1,8 @@
-import requests as req
+# import requests as req
 import json
 
 url = "https://api.twelvedata.com/time_series?apikey=74ba75970dee40e087c8f3f4ff52ed6b&symbol=LMT&interval=1day&exchange=NYSE&type=stock&start_date=2021-02-23 20:27:00&end_date=2023-09-26 20:27:00&format=json"
 
-
-
-'''
-Alemanes:
- "2019-09-11": {
-            "1. open": "115.1500",
-            "2. high": "117.2000",
-            "3. low": "115.1500",
-            "4. close": "116.5000",
-            "5. volume": "119903"
-},
-Americanos:
-        {
-            "datetime": "2023-09-26",
-            "open": "410.63000",
-            "high": "411.26999",
-            "low": "407.87360",
-            "close": "409.80991",
-            "volume": "546037"
-
-}
-result:
-{
-    {
-    "RHM": [float...], ASC
-    "LMT": [float...] ASC
-    }
-}
-'''
 data = {"RHM":[], "LMT":[]}
 with open("LMT.json") as file:
     lmt = json.load(file) # load loads dump dumps utf-8
@@ -40,7 +11,7 @@ with open("LMT.json") as file:
     def return_datetime(stock):
         return stock["datetime"]
     values = sorted(lmt["values"], key=return_datetime)
-    values = [float(stock["close"]) for stock in values]
+    values = [{f"{stock['datetime']}":float(stock["close"])} for stock in values]
     data["LMT"] = values
 
 with open("RHM.json") as file:
@@ -49,12 +20,12 @@ with open("RHM.json") as file:
     def return_datetime(stock:tuple) -> str:
         return stock[0]
     values = sorted(values.items(), key=return_datetime)
-    values = [float(stock[1]["4. close"]) for stock in values if stock[0] >= "2021-02-24"]
+    values = [{f"{stock[0]}":float(stock[1]["4. close"])} for stock in values if stock[0] >= "2021-02-24"]
     data["RHM"] = values
 
-file = open("data.json", "w")
-json.dump(data, file)
-file.close()
+# file = open("data.json", "w")
+# json.dump(data, file)
+# file.close()
 
 
 '''
@@ -69,16 +40,127 @@ Análisis Rheinmetall:
 5. Calcular R de Pearson
 '''
 
-"1. Calculas las medias de las acciones desde el 24/02/2021 - 23/02/2022 y desdel el 24/02/2022 - 26/09/2023"
+"1. Calculas las medias de las acciones desde el [24/02/2021 - 23/02/2022] y desdel el [24/02/2022 - 26/09/2023] - Rheinmetall"
+"Crear una función que acepte una fecha de incio y una fecha final"
 
 
 
 with open("data.json") as file:
     data = json.load(file)
-    print(file.closed)
-print(file.closed)
+    data = data["RHM"]
 
 
+def mean(data:list, s:str, e:str) -> float:
+    result = 0
+    len_data = 0
+    for value in data:
+        date, stock = list(value.items())[0] # 1º value: ("2021-02-24", 84.92)
+        if date >= s and date <= e:
+            result += stock
+            len_data += 1
+    return result/len_data
+
+'''
+1. Al menos un parámetro
+2. return asociado
+3. NO se pueden asignar valores
+'''
+
+values = [1,2,3,4]
+
+# 1
+def b_map(data:list) -> list:
+    result = []
+    for num in data:
+        result.append(num)
+    return result
+
+def return_value(value):
+    return value
+
+# 2
+# print(list(map(return_value,values))) # 1. for 2. append 3. return
+
+# 3
+# print(list(map(lambda value: value,values))) # 1. for 2. append 3. return 4. Ejecucción la función
+
+# 4
+
+def my_map(fun, data:list) -> list:
+    result = []
+    for value in data:
+        result.append(fun(value))
+    return result
+
+c = my_map(lambda value: value,values)
+
+# 5
+def my_map_gen(fun, data:list):
+    for value in data:
+        yield fun(value)
+
+d = my_map_gen(lambda value:value, values)
 
 
+values = [1,2,3,4]
+values = list(map(lambda value:value**2, values))
 
+# result = list(filter(lambda value: value % 2 == 0,values))
+# users = [{}]
+
+# def divide_users(users:list, group:int) -> list:
+#     result = []
+#     for user in users:
+#         if user["group"] == group:
+#             result.append(user)
+#     return result
+
+# list(filter(lambda user:user["group"] == 1, users)) # 1. for, 2. append, 3. if keyword, 4. return
+
+# print(result)
+
+
+def mean(data:list, s:str, e:str) -> float:
+    result = 0
+    len_data = 0
+    for value in data:
+        date, stock = list(value.items())[0] # 1º value: ("2021-02-24", 84.92)
+        if date >= s and date <= e:
+            result += stock
+            len_data += 1
+    return result/len_data
+
+mean_basic = mean(data, "2022-02-24", "2023-09-26")
+print(mean_basic)
+
+# lista con los precios dentro del intervalo
+# sum(lista)/len(lista)
+
+'''
+stock_dict = {
+            "2021-02-24": 84.92
+}
+
+list(stock_dict.keys())[0]
+'''
+s,e = "2021-02-24", "2022-02-23"
+# stock_price_bw = filter(lambda stock_dict: list(stock_dict.keys())[0] >= s and list(stock_dict.keys())[0] <= e, data) # 1. for
+# stock_price_bw = list(map(lambda stock_dict: list(stock_dict.values())[0], stock_price_bf))
+# print(sum(stock_price_bw)/len(stock_price_bw))
+
+
+def stock_price_by_interval(data:list, s:str, e:str) -> list:
+
+    def return_stock_price_if_date(stock_dict:dict) -> float:
+        date, stock = list(stock_dict.items())[0]
+        if date >= s and date <= e:
+            return stock
+
+    return [value for value in map(return_stock_price_if_date,data) if value]
+
+stock_before_war = stock_price_by_interval(data, s,e)
+stock_after_war = stock_price_by_interval(data, "2022-02-24","2023-09-26")
+
+mean_bw = sum(stock_before_war)/len(stock_before_war)
+mean_af = sum(stock_after_war)/len(stock_after_war)
+print(mean_bw, mean_af)
